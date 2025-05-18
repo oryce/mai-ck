6dbafcff0399dd9e260657324b93ca05ecbf57e6
kalane15 <kalane15@yandex.ru>
2025-05-18 13:49:02 +0300

    add keycloack to docker

--- code/config/postgres/init-db.sh
+++ code/config/postgres/init-db.sh
@@ -4,5 +4,7 @@ set -e
 
 psql -v ON_ERROR_STOP=1 -v PSW="'$HIVE_USER_PASSWORD'" <<-'EOSQL'
     CREATE USER hive WITH PASSWORD :PSW;
+    CREATE USER keycloak WITH PASSWORD 'keycloak123';
     CREATE DATABASE hive OWNER hive;
+    CREATE DATABASE keycloak OWNER keycloak;
 EOSQL
